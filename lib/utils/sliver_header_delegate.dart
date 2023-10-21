019ccf9a4470525d1a11c6bd317f33cab8348ce6
Pedro Vicente <pedrov.nana@gmail.com>
2023-10-21 14:33:22 -0300

    Fix problems on home and change app icon

--- lib/utils/sliver_header_delegate.dart
+++ lib/utils/sliver_header_delegate.dart
@@ -1,10 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:mancon_app/utils/format_to_money.dart';
 import 'package:mancon_app/widgets/logo.dart';
 import 'package:mancon_app/widgets/skeleton_loading_text.dart';
 
 class PersonalSliverHeaderDelegate extends SliverPersistentHeaderDelegate {
-  final double _maxExtent = 200;
   final double _minLogoSize = 40;
   final double _maxLogoSize = 60;
   final double _minAmmountSize = 15;
@@ -15,13 +16,17 @@ class PersonalSliverHeaderDelegate extends SliverPersistentHeaderDelegate {
   double totalAmmount;
   bool loading;
 
+  final LayerLink logoLayerLink = LayerLink();
+  final LayerLink totalLayerLink = LayerLink();
+
   PersonalSliverHeaderDelegate(
       {required this.totalAmmount, this.loading = false});
 
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    double percent = -(((shrinkOffset * 100) / _maxExtent) - 100);
+    double t = shrinkOffset / maxExtent;
+
     return Container(
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.background,
@@ -30,97 +35,82 @@ class PersonalSliverHeaderDelegate extends SliverPersistentHeaderDelegate {
           bottomLeft: Radius.circular(20),
         ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
         child: Column(
           children: [
             Stack(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: 70,
-                    top: 20 * percent / 100,
-                  ),
-                  child: Align(
-                    alignment: Alignment(
-                      -(shrinkOffset > _maxExtent - 20
-                              ? _maxExtent - 20
-                              : shrinkOffset) /
-                          _maxExtent,
-                      0,
-                    ),
-                    child: Logo(
-                      size: _minLogoSize > (_maxLogoSize * percent / 100)
-                          ? _minLogoSize
-                          : (_maxLogoSize * percent / 100),
+                Align(
+                  alignment: Alignment.lerp(
+                      Alignment.topCenter, Alignment.centerLeft, t)!,
+                  child: CompositedTransformTarget(
+                    link: logoLayerLink,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Logo(
+                        size: lerpDouble(_maxLogoSize, _minLogoSize, t)!,
+                      ),
                     ),
                   ),
                 ),
-                Padding(
+                CompositedTransformFollower(
+                  link: logoLayerLink,
+                  targetAnchor: Alignment.lerp(
+                      Alignment.bottomCenter, Alignment.centerRight, t)!,
+                  followerAnchor: Alignment.lerp(
+                      Alignment.topCenter, Alignment.centerLeft, t)!,
+                  child: const Text(
+                    "MANCON",
+                    style: TextStyle(
+                      fontFamily: "Righteous",
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.lerp(
+                    Alignment.center,
+                    Alignment.topRight,
+                    t,
+                  )!,
+                  child: Padding(
                     padding: EdgeInsets.only(
-                        left: (30 * percent / 100) + 40,
-                        top: (30 * percent / 100) + 10),
-                    child: Align(
-                      alignment: Alignment(
-                        -(shrinkOffset > _maxExtent - 20
-                                ? _maxExtent - 20
-                                : shrinkOffset) /
-                            _maxExtent,
-                        0,
-                      ),
-                      child: AnimatedOpacity(
-                        duration: const Duration(milliseconds: 100),
-                        opacity: percent / 100,
-                        child: const Text(
-                          "MANCON",
-                          style: TextStyle(
-                            fontFamily: "Righteous",
-                            fontSize: 15,
-                          ),
-                        ),
-                      ),
-                    )),
-                Padding(
-                  padding: EdgeInsets.only(top: 105 * percent / 100),
-                  child: Align(
-                    alignment: Alignment(
-                      (shrinkOffset > _maxExtent - 20
-                              ? _maxExtent - 20
-                              : shrinkOffset) /
-                          _maxExtent,
-                      0,
+                      top: lerpDouble(105, 10, t)!,
+                      right: lerpDouble(0, 15, t)!,
                     ),
                     child: Text(
                       "Total gasto",
                       style: TextStyle(
                         fontFamily: "inter",
-                        fontSize:
-                            _minLabelSize > (_maxLabelSize * percent / 100)
-                                ? _minLabelSize
-                                : (_maxLabelSize * percent / 100),
+                        fontSize: lerpDouble(_maxLabelSize, _minLabelSize, t)!,
                       ),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: (115 * percent / 100) + 20),
-                  child: Align(
-                    alignment: Alignment(
-                        (shrinkOffset > _maxExtent - 20
-                                ? _maxExtent - 20
-                                : shrinkOffset) /
-                            _maxExtent,
-                        0),
+                Align(
+                  alignment: Alignment.lerp(
+                    Alignment.center,
+                    Alignment.topRight,
+                    t,
+                  )!,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      top: lerpDouble(130, 25, t)!,
+                      right: lerpDouble(0, 15, t)!,
+                      bottom: 20,
+                    ),
                     child: loading
                         ? const SkeletonLoadingText(width: 160, height: 35)
                         : Text(
                             formatToMoney(totalAmmount),
                             style: TextStyle(
                               fontFamily: "Inter",
-                              fontSize: _minAmmountSize >
-                                      (_maxAmmountSize * percent / 100)
-                                  ? _minAmmountSize
-                                  : (_maxAmmountSize * percent / 100),
+                              fontSize: lerpDouble(
+                                _maxAmmountSize,
+                                _minAmmountSize,
+                                t,
+                              )!,
                             ),
                           ),
                   ),
@@ -134,7 +124,7 @@ class PersonalSliverHeaderDelegate extends SliverPersistentHeaderDelegate {
   }
 
   @override
-  double get maxExtent => _maxExtent;
+  double get maxExtent => kToolbarHeight * 3.8;
 
   @override
   double get minExtent => kToolbarHeight;
